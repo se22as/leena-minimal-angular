fa5651a76155df9fa757f666f2e18994d3f50839
smaslin <sarah.maslin@oracle.com>
2020-09-03 18:25:21 +0100

    Make the same change to server.js as made to blog angular ssr regarding not specifying APP_BASE_HREF

--- server.ts
+++ server.ts
@@ -5,7 +5,6 @@ import * as express from 'express';
 import { join } from 'path';
 
 import { AppServerModule } from './src/main.server';
-import { APP_BASE_HREF } from '@angular/common';
 import { existsSync } from 'fs';
 
 // The Express app is exported so that it can be used by serverless Functions.
@@ -35,7 +34,7 @@ export function app() {
 
   // All regular routes use the Universal engine
   server.get('*', (req, res) => {
-    res.render(indexHtml, { req, providers: [{ provide: APP_BASE_HREF, useValue: req.baseUrl }] });
+    res.render(indexHtml, { req });
   });
 
   return server;
